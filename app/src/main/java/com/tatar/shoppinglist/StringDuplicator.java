e9ae308be4f580e64cda92f81b0741ec96205d0b
tartarJR <mustafaogun.ozturk@gmail.com>
2018-07-13 16:42:36 +0300

    writing dummy unit tests to warm up

---
+++ app/src/main/java/com/tatar/shoppinglist/StringDuplicator.java
@@ -0,0 +1,9 @@
+package com.tatar.shoppinglist;
+
+public class StringDuplicator {
+
+    public String duplicate(String s) {
+        return s + s;
+    }
+
+}
---
+++ app/src/test/java/com/tatar/shoppinglist/StringDuplicatorTest.java
@@ -0,0 +1,29 @@
+package com.tatar.shoppinglist;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StringDuplicatorTest {
+
+    StringDuplicator SUT; // system under test
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_string_duplicatedStringReturned() {
+        String result = SUT.duplicate("asd");
+        assertThat(result, is("asd" + "asd"));
+    }
+}
