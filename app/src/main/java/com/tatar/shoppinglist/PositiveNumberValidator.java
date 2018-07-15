5435e3cdab90a3b2e65386dd1d9fad25fd589330
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2018-07-15 11:24:25 +0300

    removed dummy classes and their tests

--- app/src/main/java/com/tatar/shoppinglist/PositiveNumberValidator.java
+++
@@ -1,9 +0,0 @@
-package com.tatar.shoppinglist;
-
-public class PositiveNumberValidator {
-
-    public boolean isPositive(int number) {
-        return number > 0;
-    }
-
-}
--- app/src/main/java/com/tatar/shoppinglist/StringDuplicator.java
+++
@@ -1,9 +0,0 @@
-package com.tatar.shoppinglist;
-
-public class StringDuplicator {
-
-    public String duplicate(String s) {
-        return s + s;
-    }
-
-}
