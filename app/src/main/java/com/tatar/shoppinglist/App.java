a6b37024620ab83c4b28b36fbeb82c31b3248bf6
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2018-07-15 13:30:16 +0300

    init Realm

--- app/src/main/java/com/tatar/shoppinglist/App.java
+++ app/src/main/java/com/tatar/shoppinglist/App.java
@@ -2,10 +2,24 @@ package com.tatar.shoppinglist;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // initialize Realm
+        Realm.init(getApplicationContext());
+
+        // configure Realm for the app
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name("shopping_list.realm")
+                .build();
+
+        // make the above configuration default for Realm
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 }
