4ae9726c1ce798736fced13643a3460b7c1a186e
tartarJR <mustafaogun.ozturk@gmail.com>
2018-07-17 09:46:57 +0300

    added a navigation button to MainActivity

--- app/src/main/java/com/tatar/shoppinglist/ui/main/MainActivity.java
+++ app/src/main/java/com/tatar/shoppinglist/ui/main/MainActivity.java
@@ -1,13 +1,33 @@
-package com.tatar.shoppinglist;
+package com.tatar.shoppinglist.ui.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tatar.shoppinglist.R;
+import com.tatar.shoppinglist.ui.item.ItemsActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.itemsActivityBtn)
+    Button itemsActivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.itemsActivityBtn)
+    void goToItemsActivity() {
+        Intent intent = new Intent(MainActivity.this, ItemsActivity.class);
+        startActivity(intent);
     }
 }
