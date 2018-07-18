f23d6dd634b535b3d6bc166ca2b7ccdc79b206c4
tartarJR <mustafaogun.ozturk@gmail.com>
2018-07-18 17:12:43 +0300

    item adapter and its row layout

---
+++ app/src/main/java/com/tatar/shoppinglist/ui/item/ItemsAdapter.java
@@ -0,0 +1,71 @@
+package com.tatar.shoppinglist.ui.item;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tatar.shoppinglist.R;
+import com.tatar.shoppinglist.data.db.item.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    private List<Item> itemList;
+    private final Context context;
+
+    public ItemsAdapter(Context context) {
+        this.itemList = new ArrayList<>();
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_row, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsAdapter.ViewHolder holder, int position) {
+        Item item = itemList.get(position);
+
+        holder.itemNameTv.setText(item.getName());
+        holder.itemDot.setText(Html.fromHtml("&#8226;"));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.itemNameTv)
+        TextView itemNameTv;
+
+        @BindView(R.id.itemDot)
+        TextView itemDot;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+        notifyDataSetChanged();
+    }
+}
