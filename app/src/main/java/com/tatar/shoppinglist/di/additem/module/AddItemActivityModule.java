79a49509191d5d5f3973baaed2fda76ffdb9dd27
tartarJR <mustafaogun.ozturk@gmail.com>
2018-07-27 15:24:16 +0300

    ShoppingListItems adapter provider emthod added to module

--- app/src/main/java/com/tatar/shoppinglist/di/additem/module/AddItemActivityModule.java
+++ app/src/main/java/com/tatar/shoppinglist/di/additem/module/AddItemActivityModule.java
@@ -1,10 +1,12 @@
 package com.tatar.shoppinglist.di.additem.module;
 
 import com.tatar.shoppinglist.data.db.item.ItemDao;
+import com.tatar.shoppinglist.data.db.shoppinglist.ShoppingListDao;
 import com.tatar.shoppinglist.di.additem.scope.AddItemActivityScope;
 import com.tatar.shoppinglist.ui.shoppinglist.additem.AddItemActivity;
 import com.tatar.shoppinglist.ui.shoppinglist.additem.AddItemPresenterImpl;
 import com.tatar.shoppinglist.ui.shoppinglist.additem.ItemActvAdapter;
+import com.tatar.shoppinglist.ui.shoppinglist.additem.ShoppingListItemsAdapter;
 
 import dagger.Module;
 import dagger.Provides;
@@ -37,13 +39,19 @@ public class AddItemActivityModule {
 
     @AddItemActivityScope
     @Provides
-    public AddItemPresenter addItemPresenter(AddItemView addItemView, ItemDao itemDao) {
-        return new AddItemPresenterImpl(addItemView, itemDao);
+    public AddItemPresenter addItemPresenter(AddItemView addItemView, ItemDao itemDao, ShoppingListDao shoppingListDao) {
+        return new AddItemPresenterImpl(addItemView, itemDao, shoppingListDao);
     }
 
     @AddItemActivityScope
     @Provides
     public ItemActvAdapter itemActvAdapter() {
         return new ItemActvAdapter(addItemActivity);
+    }
+
+    @AddItemActivityScope
+    @Provides
+    public ShoppingListItemsAdapter shoppingListItemsAdapter() {
+        return new ShoppingListItemsAdapter();
     }
 }
