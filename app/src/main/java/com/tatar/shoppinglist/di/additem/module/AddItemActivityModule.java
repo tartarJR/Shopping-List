948c763d3f9498080dade57cc96121127996a8a2
tartarJR <mustafaogun.ozturk@gmail.com>
2018-07-27 13:56:48 +0300

    add item dagger assets

---
+++ app/src/main/java/com/tatar/shoppinglist/di/additem/module/AddItemActivityModule.java
@@ -0,0 +1,49 @@
+package com.tatar.shoppinglist.di.additem.module;
+
+import com.tatar.shoppinglist.data.db.item.ItemDao;
+import com.tatar.shoppinglist.di.additem.scope.AddItemActivityScope;
+import com.tatar.shoppinglist.ui.shoppinglist.additem.AddItemActivity;
+import com.tatar.shoppinglist.ui.shoppinglist.additem.AddItemPresenterImpl;
+import com.tatar.shoppinglist.ui.shoppinglist.additem.ItemActvAdapter;
+
+import dagger.Module;
+import dagger.Provides;
+
+import static com.tatar.shoppinglist.ui.shoppinglist.additem.AddItemContract.AddItemPresenter;
+import static com.tatar.shoppinglist.ui.shoppinglist.additem.AddItemContract.AddItemView;
+
+@Module
+public class AddItemActivityModule {
+
+    private AddItemActivity addItemActivity;
+    private AddItemView addItemView;
+
+    public AddItemActivityModule(AddItemActivity addItemActivity, AddItemView addItemView) {
+        this.addItemActivity = addItemActivity;
+        this.addItemView = addItemView;
+    }
+
+    @AddItemActivityScope
+    @Provides
+    public AddItemActivity addItemActivity() {
+        return addItemActivity;
+    }
+
+    @AddItemActivityScope
+    @Provides
+    public AddItemView addItemView() {
+        return addItemView;
+    }
+
+    @AddItemActivityScope
+    @Provides
+    public AddItemPresenter addItemPresenter(AddItemView addItemView, ItemDao itemDao) {
+        return new AddItemPresenterImpl(addItemView, itemDao);
+    }
+
+    @AddItemActivityScope
+    @Provides
+    public ItemActvAdapter itemActvAdapter() {
+        return new ItemActvAdapter(addItemActivity);
+    }
+}
