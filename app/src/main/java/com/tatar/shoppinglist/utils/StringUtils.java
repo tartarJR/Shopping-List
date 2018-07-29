4891b2ab0d45ea60abfbe8eb6a5ef1ec481fb374
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2018-07-29 20:02:51 +0300

    generating a fake id via DigestUtils lib

--- app/src/main/java/com/tatar/shoppinglist/utils/StringUtils.java
+++ app/src/main/java/com/tatar/shoppinglist/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package com.tatar.shoppinglist.utils;
 
 
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.text.WordUtils;
+
+import java.util.UUID;
 
 public class StringUtils {
 
@@ -13,6 +17,17 @@ public class StringUtils {
         String normalizedString = name.trim().replaceAll("\\s+", " ");
 
         return WordUtils.capitalizeFully(normalizedString);
+    }
+
+    /*
+     * Creates a fake id that is used to determine which lists belong to which users in Firebase.
+     * Just a work around for authentication since I did not have time to implement a real authentication via Firebase.
+     */
+    public static String generateFakeUserId() {
+        String ts = String.valueOf(System.currentTimeMillis());
+        String rand = UUID.randomUUID().toString();
+
+        return new String(Hex.encodeHex(DigestUtils.sha(ts + rand)));
     }
 
 }
