db5435ffc29c3e8af11955b7f37008032cefbea2
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2018-07-29 20:04:50 +0300

    generating a fake user id on App's onCreate

--- app/src/main/java/com/tatar/shoppinglist/App.java
+++ app/src/main/java/com/tatar/shoppinglist/App.java
@@ -3,9 +3,11 @@ package com.tatar.shoppinglist;
 import android.app.Activity;
 import android.app.Application;
 
+import com.tatar.shoppinglist.data.prefs.SharedPreferencesManager;
 import com.tatar.shoppinglist.di.app.component.AppComponent;
 import com.tatar.shoppinglist.di.app.component.DaggerAppComponent;
 import com.tatar.shoppinglist.di.app.module.ContextModule;
+import com.tatar.shoppinglist.utils.StringUtils;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -14,6 +16,7 @@ import timber.log.Timber;
 public class App extends Application {
 
     private AppComponent appComponent;
+    private SharedPreferencesManager sharedPreferencesManager;
 
     @Override
     public void onCreate() {
@@ -33,6 +36,13 @@ public class App extends Application {
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
                 .name("shopping_list.realm")
                 .build();
+
+        /*
+         * Generate and set a fake user id.
+         * Just a work around for authentication since I did not have time to implement a real authentication via Firebase.
+         */
+        sharedPreferencesManager = appComponent.sharedPreferencesManager();
+        sharedPreferencesManager.setUserId(StringUtils.generateFakeUserId());
 
         // make the above configuration default for Realm
         Realm.setDefaultConfiguration(realmConfiguration);
