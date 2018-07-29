0eeb3f77df3a22da03ed2dedc94328f39e9eb3d7
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2018-07-29 17:30:35 +0300

    made network models parcelable

--- app/src/main/java/com/tatar/shoppinglist/data/network/model/ShoppingList.java
+++ app/src/main/java/com/tatar/shoppinglist/data/network/model/ShoppingList.java
@@ -1,12 +1,15 @@
 package com.tatar.shoppinglist.data.network.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 
-public class ShoppingList {
+public class ShoppingList implements Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -23,6 +26,25 @@ public class ShoppingList {
     @SerializedName("user_id")
     @Expose
     private String userId;
+
+    protected ShoppingList(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+        shoppingListItems = in.createTypedArrayList(ShoppingListItem.CREATOR);
+        userId = in.readString();
+    }
+
+    public static final Creator<ShoppingList> CREATOR = new Creator<ShoppingList>() {
+        @Override
+        public ShoppingList createFromParcel(Parcel in) {
+            return new ShoppingList(in);
+        }
+
+        @Override
+        public ShoppingList[] newArray(int size) {
+            return new ShoppingList[size];
+        }
+    };
 
     public String getId() {
         return id;
@@ -54,5 +76,18 @@ public class ShoppingList {
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(name);
+        parcel.writeTypedList(shoppingListItems);
+        parcel.writeString(userId);
     }
 }
--- app/src/main/java/com/tatar/shoppinglist/data/network/model/ShoppingListItem.java
+++ app/src/main/java/com/tatar/shoppinglist/data/network/model/ShoppingListItem.java
@@ -1,9 +1,12 @@
 package com.tatar.shoppinglist.data.network.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class ShoppingListItem {
+public class ShoppingListItem implements Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -14,6 +17,24 @@ public class ShoppingListItem {
     @SerializedName("is_collected")
     @Expose
     private boolean isCollected;
+
+    protected ShoppingListItem(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+        isCollected = in.readByte() != 0;
+    }
+
+    public static final Creator<ShoppingListItem> CREATOR = new Creator<ShoppingListItem>() {
+        @Override
+        public ShoppingListItem createFromParcel(Parcel in) {
+            return new ShoppingListItem(in);
+        }
+
+        @Override
+        public ShoppingListItem[] newArray(int size) {
+            return new ShoppingListItem[size];
+        }
+    };
 
     public String getId() {
         return id;
@@ -37,5 +58,17 @@ public class ShoppingListItem {
 
     public void setCollected(boolean collected) {
         isCollected = collected;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(name);
+        parcel.writeByte((byte) (isCollected ? 1 : 0));
     }
 }
